2c8391b9a2f664940647fe9dade5f593382ab0b4
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-08 14:39:12 +0100

    getting spec running

---
+++ Source/commands/for_CommandCustomAttribute/when_button_gets_clicked_and_command_is_set.js
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { CommandCustomAttribute } from '../CommandCustomAttribute';
+import { Command } from '@dolittle/commands';
+
+describe("when button gets clicked and command is set", () => {
+    let attribute = null;
+    let element = null;
+    let commandCoordinator = null;
+    let command = null;
+
+    (beforeEach(() => {
+        element = sinon.stub();
+        commandCoordinator = {
+            handle: sinon.stub().returns({
+                then: sinon.stub()
+            })
+        }
+        command = new Command();
+        attribute = new CommandCustomAttribute(element, commandCoordinator);
+
+        attribute.commandChanged(command);
+        element.onclick();
+    }));
+
+    it("should pass the command along to handle", () => commandCoordinator.handle.calledWith(command).should.be.true);
+});
