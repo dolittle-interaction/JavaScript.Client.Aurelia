7ae574b0d1195856effa5f646b48bab463f9dfbc
Pavneet Singh Saund <pav.saund@gmail.com>
2019-01-11 10:01:39 +0100

    Update dolittle build dependencies to specific basic version

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const config = require('dolittle.javascript.build.aurelia/webpack.config.js');
+const config = require('@dolittle/build.aurelia/webpack.config.js');
 config.entry = {
     app: ['babel-polyfill', 'aurelia-bootstrapper'],
     vendor: ['bluebird']
