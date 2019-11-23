f8d0ae68466568094d5a2a262364409f94c20877
Sindre Wilting <sindrewilting@gmail.com>
2019-11-23 16:43:26 +0100

    Root configurations

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -2,5 +2,6 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-require('@dolittle/build/dist/gulp/setup')(exports);
+const build = require('@dolittle/typescript.build');
 
+module.exports = build.wallaby();
